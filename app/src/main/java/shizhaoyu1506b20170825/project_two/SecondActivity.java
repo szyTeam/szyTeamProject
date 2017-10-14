edfd6a1bb9ebc2c21c4f1e0a84ded050dfbf900c
TigerIs <328304262@qq.com>
2017-10-14 10:21:28 +0800

    log

--- app/src/main/java/shizhaoyu1506b20170825/project_two/SecondActivity.java
+++ app/src/main/java/shizhaoyu1506b20170825/project_two/SecondActivity.java
@@ -3,6 +3,7 @@ package shizhaoyu1506b20170825.project_two;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * 类用途：
@@ -13,5 +14,7 @@ public class SecondActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.e("","");
     }
 }
